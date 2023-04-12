4f41a310601a0a6f15c2510833c7a9eefa374ba1
Perekeme <108529208+Perekeme@users.noreply.github.com>
2023-04-12 18:13:17 +0200

    practice

---
+++ array.py
@@ -0,0 +1,13 @@
+# array.py
+
+new_list = [1,2,3]
+# result = new_list[5]
+
+# print(result)
+print(7 in new_list) 
+
+new_list.append(4)
+print(new_list)
+new_list.insert(0,0)
+new_list.extend([5,6,7])
+print(new_list)
